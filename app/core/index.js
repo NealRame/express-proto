642c53921391eb95eb6ba858f37b3734142cfa35
NealRame <contact@nealrame.com>
2015-03-22 23:09:24 +0100

    first commit

---
+++ app/core/index.js
@@ -0,0 +1,72 @@
+var _ = require('underscore');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var path = require('path');
+
+var app;
+
+exports.instance = function() {
+    if (app) {
+        return Promise.resolve(app);
+    }
+
+    var config = require('core/config');
+
+    return (new Promise(function(resolve, reject) {
+        mongoose.connect(config.database.fullURI).connection
+            .once('open', resolve)
+            .once('error', reject);
+    })).then(function(conn) {
+        var app = express();
+
+        app.set('config', config);
+
+        // view engine setup
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('view engine', 'jade');
+
+        // uncomment after placing your favicon in /public
+        //app.use(favicon(__dirname + '/public/favicon.ico'));
+        app.use(logger('dev'));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(cookieParser());
+
+        app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+
+        // app.use('/', require('core/routes'));
+
+        // catch 404 and forward to error handler
+        app.use(function(req, res, next) {
+            var err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        });
+
+        // error handlers
+        app.use(function(err, req, res, next) {
+            var error = _.defaults(
+                _.pick(err, 'message', 'status'), {
+                    message: 'Internal server error',
+                    status: 500
+                }
+            );
+
+            if (app.get('env') === 'development' && err.stack) {
+                error.stack = err.stack;
+            }
+            res.status(error.status);
+            if (req.api) {
+                res.send(error);
+            } else {
+                res.render('error', {error: error});
+            }
+        });
+
+        return app;
+    });
+};
